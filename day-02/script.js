0106eac46c8ce2439e053e78852083458e179406
cykoravish <ravishbisht85@gmail.com>
2026-04-10 11:14:09 +0530

    find max num - day-2

---
+++ day-02/script.js
@@ -0,0 +1,11 @@
+// 1. Find the maximum of three numbers
+// input: 10 20 5
+// output: 20
+
+// function findMax(num1, num2, num3){
+//     let max = num1;
+//     if(num2 > max)max = num2; 
+//     if(num3 > max)max = num3;
+//     return max;
+// }
+// console.log(findMax(10, 20, 5));
